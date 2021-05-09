8e89e731356740ef6ceca8818b803683ad5757d9
Brenden Ferguson <brenden.ferguson@gmail.com>
2021-05-08 17:55:31 -0700

    adds compile script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 deno compile --unstable --lite --output build/pig_linux --target x86_64-unknown-linux-gnu main.ts
-deno compile --unstable --lite --output build/pig_windows.exe --target x86_64-pc-windows-msvc main.ts
+deno compile --unstable --lite --output build/pig_windows --target x86_64-pc-windows-msvc main.ts
 deno compile --unstable --lite --output build/pig_mac --target x86_64-apple-darwin main.ts
 deno compile --unstable --lite --output build/pig_m1_mac --target aarch64-apple-darwin main.ts
